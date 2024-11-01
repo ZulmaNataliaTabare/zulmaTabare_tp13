87cc59f6998dc2ddb3df4640c32384181bff168a
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-11-01 16:19:37 -0300

    Modifiqué archivo

--- bankBalance.js
+++ bankBalance.js
@@ -1,2 +1,46 @@
 
+// 1. En Visual Studio Code crear la carpeta de trabajo y dentro de ella inicializar un
+// repositorio local y vincularlo con un repositorio remoto cuyo nombre debe
+// tener la siguiente nomenclatura: nombreApellido_tp13
+// 2. Crear un archivo llamado bankBalance.js
+// 3. Crear un array que contenga las operaciones bancarias realizadas por el cliente,
+// tomando en cuenta que los depósitos serán representados por valores
+// positivos y los retiros por valores negativos.
+
 let operaciones = [5000, -2500, 158000, 65000, 47000, -35000, 88000, -25000, 115500]
+
+// 4. Crear la función calculateBalances() que reciba como parámetro el array
+// creado. La función tendrá la responsabilidad de calcular y devolver el saldo total
+// de depósitos, el saldo actual de retiros y el saldo actual del cliente
+
+function calculateBalances(operaciones){
+
+//con Filter() creo un array nuevo con todos los valores positivos/negativos y con Reduce() los reduzco a un solo valor.   
+
+let depósitos = operaciones.filter(num => num > 0);
+
+let saldoTotalDepósitos = depósitos.reduce((acum,num) => acum + num, 0);
+
+let mensajeSaldoCliente= `El saldo total de depósitos es: $${saldoTotalDepósitos}`;
+    
+
+let retiros = operaciones.filter(num => num < 0);
+
+let saldoActualRetiros = retiros.reduce((acum,num) => acum + num, 0);
+
+let mensajeRetiroCliente = `El saldo actual de retiros es: $${saldoActualRetiros}`;
+    
+let saldoActualCliente = (saldoTotalDepósitos + saldoActualRetiros);
+
+let mensajeSaldoActualCliente = `El saldo actual del cliente es: $${saldoActualCliente}`;
+
+
+return {
+    mensajeSaldoCliente,
+    mensajeRetiroCliente,
+    mensajeSaldoActualCliente
+    };
+
+}
+
+console.log(calculateBalances(operaciones));
